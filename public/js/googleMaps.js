74e3084c0cdd2e9a66ca0add0f75c054b57a602e
pattyshao <pshao@ucsd.edu>
2018-02-28 22:42:06 -0800

    thirdpartyAPI lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,18 @@
 function initMap() {
 	// add your code here
+	var ucsd_ltlng = {lat: 32.880, lng:-117.236};
+	var ssc = {lat:32.879012, lng:-117.235844};
+
+	//create a map object and specify the DOM element for display.
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		postion: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+	});
+
 }
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -18,9 +18,29 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function()){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function taoholdHandler(event){
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: " + targetIDPrefix);
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e)) {
+  	e.preventDefault();
+  	console.log("Subtmitting form...");
+  	var rsvpEmail = $('#rsvpEmail').val();
+  	$.post('addRSVP', {rsvpEmail:rsvpEmail}, postCallback);
+
+  });
+	function postCallback(res) {
+		alert("RSVP was successfully submitted!");
+		$('#rsvpEmail').val('');
+	}
 }
